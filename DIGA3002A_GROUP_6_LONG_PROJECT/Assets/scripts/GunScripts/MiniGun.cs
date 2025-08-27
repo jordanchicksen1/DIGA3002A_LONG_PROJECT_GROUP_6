4c509e539c9609d48ebc941722d44654172b9add
RamzCas <2749414@students.wits.ac.za>
2025-08-27 15:45:54 +0200

    Mini Gun start

---
+++ DIGA3002A_GROUP_6_LONG_PROJECT/Assets/scripts/GunScripts/MiniGun.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGun : MonoBehaviour
+{
+    public bool Shooting;
+    void Update()
+    {
+        
+    }
+}
