6a0a1735fc13fd6a9c6b3c3db1c046f18fb36710
RamzCas <2749414@students.wits.ac.za>
2025-09-23 15:46:33 +0200

    music start

---
+++ DIGA3002A_GROUP_6_LONG_PROJECT/Assets/scripts/MusicManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    public AudioSource BackgroundMusic;
+    public AudioSource SFX;
+
+    [Header("Audio Clips")]
+    public AudioClip RifleFire;
+}
--- DIGA3002A_GROUP_6_LONG_PROJECT/Assets/scripts/ParticalTrigger.cs
+++ DIGA3002A_GROUP_6_LONG_PROJECT/Assets/scripts/ParticalTrigger.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ParticalTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerStay(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       
     }
 }
