c5ca1ef6258b0a401a9fb77d65bd75c6e13387d1
jordanchicksen1 <2333640@students.wits.ac.za>
2025-11-12 02:33:19 +0200

    last boss stuff

---
+++ DIGA3002A_GROUP_6_LONG_PROJECT/Assets/scripts/lastBoss/lastBossHealth.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lastBossHealth : MonoBehaviour
+{
+
+    [Header("Health Bar")]
+    public float maxHealth = 1500f;
+    public float currentHealth;
+    public Image healthBarPic;
+    public lastBossPosture lastBossPosture;
+    public lastBoss firstBoss;
+    public ParticleSystem explosion;
+    public static event System.Action<lastBossHealth> OnBossLDeath;
+    public void Start()
+    {
+        currentHealth = maxHealth;
+
+
+    }
+
+    public void Update()
+    {
+        if (currentHealth > 1000f)
+        {
+            firstBoss.Phase1 = true;
+            firstBoss.Phase2 = false;
+            firstBoss.Phase3 = false;
+        }
+
+        if (currentHealth < 1000f && currentHealth > 500f)
+        {
+            firstBoss.Phase1 = false;
+            firstBoss.Phase2 = true;
+            firstBoss.Phase3 = false;
+        }
+
+        if (currentHealth < 500f)
+        {
+            firstBoss.Phase1 = false;
+            firstBoss.Phase2 = false;
+            firstBoss.Phase3 = true;
+        }
+
+        if (currentHealth <= 0f)
+        {
+            Debug.Log("Boss died");
+            if (explosion != null)
+            {
+                ParticleSystem ps = Instantiate(explosion, transform.position, Quaternion.identity);
+                ps.Play();
+                Destroy(ps.gameObject, ps.main.duration);
+            }
+
+            OnBossLDeath?.Invoke(this);
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    [ContextMenu("BasicHit")]
+    public void BasicHit()
+    {
+        currentHealth = currentHealth - 5f;
+        updateHealthBar();
+        lastBossPosture.PostureHitALot();
+    }
+
+    [ContextMenu("MachineHit")]
+    public void MachineHit()
+    {
+        currentHealth = currentHealth - 2f;
+
+        updateHealthBar();
+        lastBossPosture.SmallPostureHit();
+    }
+
+    [ContextMenu("AssaultHit")]
+
+    public void AssaultHit()
+    {
+        currentHealth = currentHealth - 4f;
+        updateHealthBar();
+        lastBossPosture.PostureHitALot();
+    }
+
+    [ContextMenu("LaserHit")]
+    public void LaserHit()
+    {
+        currentHealth = currentHealth - 10f;
+
+        updateHealthBar();
+        lastBossPosture.BigPostureHit();
+    }
+
+    [ContextMenu("BeamHit")]
+    public void BeamHit()
+    {
+        currentHealth = currentHealth - 50f;
+
+        updateHealthBar();
+        lastBossPosture.BigPostureHit();
+    }
+
+    public void updateHealth(float amount)
+    {
+        currentHealth += amount;
+        updateHealthBar();
+
+    }
+
+    public void updateHealthBar()
+    {
+        float targetFillAmount = currentHealth / maxHealth;
+        healthBarPic.fillAmount = targetFillAmount;
+    }
+}
---
+++ DIGA3002A_GROUP_6_LONG_PROJECT/Assets/scripts/missionManagers/lastBossMission.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class lastBossMission : MonoBehaviour
+{
+    public int requiredKills = 1;   // how many enemies must be killed
+    public int currentKills = 0;
+    public TextMeshProUGUI currentKillsText;
+    public GameObject currentKillsTextObject;
+    public GameObject mission1CompleteScreen;
+    public GameObject player;
+    public Transform garageTeleporter;
+    public CharacterController characterController;
+    public GameObject globalVolumes;
+    public GameObject levelMusic;
+    public playerHealth playerHealth;
+    public uiButtons uiButtons;
+    public GameObject lobbyMusic;
+    public GameObject boss1Icon;
+    public credits credits;
+    private void OnEnable()
+    {
+        firstBossHealth.OnBoss1Death += HandleBossDeath;
+    }
+
+    private void OnDisable()
+    {
+        firstBossHealth.OnBoss1Death -= HandleBossDeath;
+    }
+
+    private void HandleBossDeath(firstBossHealth boss)
+    {
+        CountKill();
+        Debug.Log("MissionManager received TankDeath event!");
+    }
+
+
+
+    private void CountKill()
+    {
+        currentKills++;
+        currentKillsText.text = currentKills.ToString();
+        Debug.Log("Enemy destroyed! Total: " + currentKills);
+
+        if (currentKills >= requiredKills)
+        {
+            MissionComplete();
+        }
+    }
+
+    public void MissionComplete()
+    {
+        Debug.Log("Mission Complete!");
+        StartCoroutine(MissionOneComplete());
+    }
+
+    public IEnumerator MissionOneComplete()
+    {
+        yield return new WaitForSeconds(2f);
+        mission1CompleteScreen.SetActive(true);
+        characterController.enabled = false;
+        yield return new WaitForSeconds(2f);
+        player.transform.position = garageTeleporter.transform.position;
+        characterController.enabled = true;
+        mission1CompleteScreen.SetActive(false);
+        playerHealth.Heal();
+        globalVolumes.SetActive(false);
+        levelMusic.SetActive(false);
+        uiButtons.isDoingBossOne = false;
+        currentKillsTextObject.SetActive(false);
+        lobbyMusic.SetActive(true);
+        boss1Icon.SetActive(false);
+        credits.AddBossOneCredits();
+    }
+}
